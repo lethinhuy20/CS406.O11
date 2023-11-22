21d6a1e2b54af9e7bd17bda97da14c6309187e1c
Lê Thị Như Ý <93437109+lethinhuy20@users.noreply.github.com>
2023-11-22 20:11:22 +0700

    Delete webthoitrang directory

--- webthoitrang/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-#new
-from app.models import UploadFile
-# Register your models here.
-
-admin.site.register(UploadFile)
--- webthoitrang/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-from app.views import file_upload_view
-urlpatterns = [
-    path('', views.home),
-    path('upload/',file_upload_view)
-]
